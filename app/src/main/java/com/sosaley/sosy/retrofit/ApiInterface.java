66e88544e2d3744d15aa6a4486e2f235398ef6fd
murali <murali.e@sosaley.in>
2021-02-26 13:19:33 +0530

    Retrofit Changes Commit

---
+++ app/src/main/java/com/sosaley/sosy/retrofit/ApiInterface.java
@@ -0,0 +1,25 @@
+package com.sosaley.sosy.retrofit;
+
+
+
+public interface ApiInterface {
+
+    //Login
+   /* @Headers({"Content-Type:application/json"})
+    @POST("get_single_user.php")
+    Call<SignedInJSONResponse> getLoginUser(@Body SignInDTO signInDTO);
+*/
+
+   /*@GET(BuildConfig.NAJDI_END_POINTS + "products")
+    @Headers({"Content-Type:application/json", "Authorization" + ": " + Constants.BASIC_64_AUTH})
+    Call<List<ProductListResponse>> getProducts();
+*/
+
+    /*@POST(BaseURL.DOMAIN_NAME + "register")
+    @Headers({"Content-Type:application/json"})
+    Call<BaseResponse> registerRider(@Body RegisterRiderRequest registerRiderRequest);
+*/
+
+
+
+}
