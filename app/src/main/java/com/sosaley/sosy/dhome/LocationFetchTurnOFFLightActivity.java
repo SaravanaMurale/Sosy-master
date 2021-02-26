7990d9374e6bc074a40f95cd7300c5e7ddfc4ea1
murali <murali.e@sosaley.in>
2021-02-26 13:22:51 +0530

    Retrofit Changes Commit

--- app/src/main/java/com/sosaley/sosy/dhome/LocationFetchTurnOFFLightActivity.java
+++ app/src/main/java/com/sosaley/sosy/dhome/LocationFetchTurnOFFLightActivity.java
@@ -2,15 +2,61 @@ package com.sosaley.sosy.dhome;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
+import android.view.View;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
 import com.sosaley.sosy.R;
 
-public class LocationFetchTurnOFFLightActivity extends AppCompatActivity {
+public class LocationFetchTurnOFFLightActivity extends AppCompatActivity implements OnMapReadyCallback {
+
+    private GoogleMap mGoogleMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+    private View mapView;
+
+    private Location mLastKnownLocation;
+    private LocationCallback locationCallback;
+
+    private final float DEFAULT_ZOOM = 13;
+
+    private LocationManager locationManager;
+    Double myLocationLat, myLocationLon;
+
+    public boolean isGPS;
+
+    MarkerOptions myCurrentLocation, deliveryLocation;
+    Marker myCurrentLocationMarker, deliveryLocationMarker;
+    Polyline currentPolyLine;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_location_fetch_turn_o_f_f_light);
+
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        mapView = mapFragment.getView();
+
+        mFusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(LocationFetchTurnOFFLightActivity.this);
+
+        locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
+
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
     }
 }
